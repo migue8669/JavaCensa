c69f9172ec7b7cd9a984e6c57a36a05d84815480
migue8669 <miguelramirez8669@gmail.com>
2021-02-13 08:55:15 -0500

    desarrollo nota 3

--- Nota-3/src/main/java/Punto3.java
+++ Nota-3/src/main/java/Punto3.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class Punto3 {
-    public static void main(String[] args) {
-        int[] numero [10];
-    }
+    public static void main (String [ ] args) {
+        String cadena = "Centro de sistemas de antioquia";
+        int a = 0, e = 0, i = 0, o = 0, u = 0;
+
+        for (int x = 0; x < cadena.length(); x++) {
+            char actual = cadena.toLowerCase().charAt(x);
+
+            switch (actual) {
+                case 'a':
+                    a++;
+                    break;
+                case 'e':
+                    e++;
+                    break;
+                case 'i':
+                    i++;
+                    break;
+                case 'o':
+                    o++;
+                    break;
+                case 'u':
+                    u++;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        System.out.println("Total de vocales: " + (a+e+i+o+u));
+
+        System.out.println("Cantidad de a: " + a);
+        System.out.println("Cantidad de e: " + e);
+        System.out.println("Cantidad de i: " + i);
+        System.out.println("Cantidad de o: " + o);
+        System.out.println("Cantidad de u: " + u);}
 }
